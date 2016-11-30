8f65f4712809d90a650759ae789294bf3af42e92
Dallin Parker <dallinparker@Dallins-MacBook-Pro.local>
2016-11-30 09:52:21 -0700

    fixing local change

--- gulpfile.js
+++ gulpfile.js
@@ -2,43 +2,43 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
 const sass = require('gulp-sass');
-// const sourcemap = require('gulp-sourcemaps');
+const sourcemap = require('gulp-sourcemaps');
 const nodemon = require('gulp-nodemon');
 // const annotate = require('gulp-ng-annotate');
 
 const paths = {
-  jsSource: ['./public/app/app.js', './public/app/**/*.js'],
-  scssSource: ['./public/app/**/*.scss'],
-  server: ['./server/server.js']
+    jsSource: ['./public/app/app.js', './public/app/**/*.js'],
+    scssSource: ['./public/app/**/*.scss'],
+    server: ['./server/server.js']
 };
 
 gulp.task('serve', () => {
-  nodemon({
-    'script': paths.server
-  })
+    nodemon({
+        'script': paths.server
+    })
 })
 
 gulp.task('js-bundle', () => {
-  gulp.src(paths.jsSource)
-  // .pipe(sourcemap.init())
-  .pipe(babel({
-    presets: ['es2015']
-  }))
-  .pipe(concat('all.js'))
-  // .pipe(sourcemap.write('./'))
-  .pipe(gulp.dest('./public/dist'))
+    gulp.src(paths.jsSource)
+        // .pipe(sourcemap.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('all.js'))
+        // .pipe(sourcemap.write('./'))
+        .pipe(gulp.dest('./public/dist'))
 });
 
 gulp.task('scss-bundle', () => {
-  gulp.src(paths.scssSource)
-  .pipe(sass())
-  .pipe(concat('styles.css'))
-  .pipe(gulp.dest('./public/dist'))
+    gulp.src(paths.scssSource)
+        .pipe(sass())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('./public/dist'))
 });
 
 gulp.task('watch', () => {
-  gulp.watch(paths.jsSource, ['js-bundle'])
-  gulp.watch(paths.scssSource, ['scss-bundle'])
+    gulp.watch(paths.jsSource, ['js-bundle'])
+    gulp.watch(paths.scssSource, ['scss-bundle'])
 });
 
 
