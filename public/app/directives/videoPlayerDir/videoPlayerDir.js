a8b817a0b6c2418571cd1b989969e9e3a832a94f
Mike <mike.chi4@gmail.com>
2016-12-02 10:53:43 -0700

    cosmetic changes

--- public/app/directives/videoPlayerDir/videoPlayerDir.js
+++ public/app/directives/videoPlayerDir/videoPlayerDir.js
@@ -33,19 +33,15 @@ angular.module('you-tube-clone')
 
           // this specific variable holds the video info (ie title, and statistics)
           $scope.videoData = response.items[0];
-
+          console.log($scope.videoData);
           const reactionSum = ($scope.videoData.statistics.likeCount * 1) + ($scope.videoData.statistics.dislikeCount * 1);
+
+          // get percent to adjust bar to show like/dislike ratio
           const percent = ($scope.videoData.statistics.likeCount / reactionSum) * 100;
-          console.log(percent);
           $('.myBar').css('width', percent + '%');
         })
       }
         $scope.getVideoInfo(id);
-
-
-
-
-
     }
   }
 
