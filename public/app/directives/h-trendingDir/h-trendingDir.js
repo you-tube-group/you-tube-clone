28e49f4e9f2fead9753f86e8f117b72cd4981b0b
dallin-r-parker <dallin.r.parker@gmail.com>
2016-12-02 12:25:29 -0700

    init

--- public/app/directives/h-trendingDir/h-trendingDir.js
+++ public/app/directives/h-trendingDir/h-trendingDir.js
@@ -54,11 +54,11 @@ angular.module('you-tube-clone')
 
 $(() =>{
 // NOTE: multiply 427 by the number of slides you want to move right or left
-    $('.arrow-wrap').on('click', () => {
+    $('.arrow-wrap-right').on('click', () => {
         $('.trending-vid-carousel').animate({"margin-left": "-=1281"}, 700);
     });
 
-    $('.arrow-wrap-right').on('click', () => {
+    $('.arrow-wrap').on('click', () => {
         $('.trending-vid-carousel').animate({"margin-right": "-=1281"}, 700);
     });
 }); //<-- end of jQuery
