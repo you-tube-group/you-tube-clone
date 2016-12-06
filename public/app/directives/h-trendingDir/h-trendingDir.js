3d1f12260e0577c6c38b8674549ab10d64b74d01
dallin-r-parker <dallin.r.parker@gmail.com>
2016-12-06 15:15:32 -0700

    working on carousel

--- public/app/directives/h-trendingDir/h-trendingDir.js
+++ public/app/directives/h-trendingDir/h-trendingDir.js
@@ -44,13 +44,13 @@ angular.module('you-tube-clone')
 
                     $(() => {
                         // NOTE: to move slide 1 place = 427 (muliply number of slides you want to move by 427)
-                        $('.arrow-wrap').on('click', () => {
+                        $('.arrow-wrap-right').on('click', () => {
                             $('.trending-vid-carousel').animate({
                                 "margin-left": "-=854"
                             }, 700);
                         });
 
-                        $('.arrow-wrap-right').on('click', () => {
+                        $('.arrow-wrap').on('click', () => {
                             $('.trending-vid-carousel').animate({
                                 "margin-right": "-=854"
                             }, 700);
